2d4d4a77932b1379eb95945c025c55dd7b6f51f0
Clair Wanjiku <clair.karanja@student.moringaschool.com>
2023-10-23 16:39:40 +0300

    completed assignment

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,11 +1,13 @@
+
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import React from "react";
 
-import Header from "../components/Header";
+import Header from "../components/Header.js"; // Import Header component
+
 
 test("displays the text 'hello from the Header!'", () => {
-  render(<Header />);
+  render(<Header/>);
 
   expect(screen.queryByText("hello from the Header!")).toBeInTheDocument();
 });
